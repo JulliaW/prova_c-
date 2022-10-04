dd7b9b2e71775d9d0c5c5d3869e248c5c9270ca8
Jullia Weber <weberjullia44@gmail.com>
2022-10-04 19:47:22 -0300

    First commit

---
+++ prova/Data/AppCont.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prova.Models;
+
+namespace prova.Data
+{
+    public class AppCont : DbContext
+    {
+        public AppCont(DbContextOptions<AppCont> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ prova/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prova.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+     
+        public DateTime DataNascimento { get; set; }
+    }
+}
